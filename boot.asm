0f8a9d091f4c2c312f5cb09076e076ef1e2ca59e
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-10-17 12:13:13 -0400

    Lowercase alphabet

--- boot.asm
+++ boot.asm
@@ -1,14 +1,14 @@
 mov ah, 0x0e
-mov al, 65
+mov al, 97
 int 0x10
 
 loop:
     inc al
-    cmp al, 'Z' + 1
+    cmp al, 'z' + 1
     je exit
     int 0x10
     jmp loop
 exit:
     jmp $
 times 510-($-$$) db 0
-db 0x55, 0xaa 
+db 0x55, 0xaa
